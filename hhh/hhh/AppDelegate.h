a381d821ae99ade634bff8e49507bd815eb3c748
李坤朋 <470473261@qq.com>
2015-07-09 09:42:45 +0800

    添加新工程，哈哈哈哈

---
+++ hhh/hhh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hhh
+//
+//  Created by 李坤朋 on 15/7/9.
+//  Copyright (c) 2015年 李坤朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hhh/hhh/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hhh
+//
+//  Created by 李坤朋 on 15/7/9.
+//  Copyright (c) 2015年 李坤朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
